9c6374d916aba7df0547776343fec9c185057531
Volkan Temel <sapphiron07@windowslive.com>
2020-11-07 20:03:01 +0300

    Enter

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-setInterval(() => {
-    
-        document.getElementById("colorPicker").innerHTML=" ."
-        setTimeout(() => {
-            document.getElementById("colorPicker").innerHTML=" .."
-            setTimeout(() => {
-                document.getElementById("colorPicker").innerHTML=" ..."
-            }, 200);
-        }, 200);
-    
-}, 600);
-
-
-
+function openForm() {
+  if (document.getElementById("hide").innerHTML === "Show") {
+    document.getElementById("project-grid").style.display = "block";
+    document.getElementById("hide").innerHTML = "Hide";
+  }
+  else{
+    document.getElementById("project-grid").style.display = "none";
+    document.getElementById("hide").innerHTML = "Show";
+  }
+}
