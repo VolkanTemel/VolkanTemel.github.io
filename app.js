002c6d8318d8eca1ac85ad4e80d8725085aa1b1a
Volkan Temel <sapphiron07@windowslive.com>
2020-11-07 20:48:57 +0300

    Enter

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ function openForm() {
   if (document.getElementById("hide").innerHTML === "Show") {
     document.getElementById("project-grid").style.display = "block";
     document.getElementById("hide").innerHTML = "Hide";
-  }
-  else{
+  } else {
     document.getElementById("project-grid").style.display = "none";
     document.getElementById("hide").innerHTML = "Show";
   }
 }
+
+
